f7167409fe2903c9b7843c6f0193c573f048318a
Michael Lamb <mflambjr@gmail.com>
2019-06-23 16:17:09 -0400

    test rendering of refactored layout/components for home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,58 @@
 <template>
   <Layout>
-    
+    <!-- <div class="parallax"> -->
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <!-- <g-image alt="Que Ricas Logo" src="~/que-ricas-logo.jpg" width="135" /> -->
-    
+      <Carousel />
+   
+   <div class="index-content-container">
 
     <h1>Hello, world!</h1>
-   
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at blanditiis temporibus voluptatem quia perferendis magnam eaque voluptates culpa recusandae porro illo praesentium reprehenderit ab, quaerat, quod dolorem deserunt doloribus consequuntur in rerum aliquid consectetur illum odio? Eum cumque illo, saepe cum ad, architecto magni, numquam iure voluptates ullam sequi libero voluptas deleniti ducimus incidunt laboriosam sed quas porro! Magni amet nobis nulla explicabo in. Repudiandae beatae saepe impedit laboriosam alias reiciendis amet voluptatibus quod, quibusdam, quo debitis explicabo sapiente sunt nobis ullam mollitia quaerat repellendus. Consequuntur adipisci tempore quas voluptas quasi earum deleniti nostrum minima quod culpa soluta magnam enim doloribus natus molestiae voluptate nihil, accusantium facere accusamus iste, at dolor hic! Neque in sapiente officiis amet ut, quam nesciunt accusantium beatae reprehenderit et unde voluptates nobis nisi illum mollitia enim aspernatur modi assumenda optio iste est aliquid! Ipsam aut alias accusamus, laboriosam nemo cupiditate eveniet magni hic repellendus modi officia delectus, nam recusandae voluptatem, vero veritatis neque deserunt similique quibusdam dolore quam unde culpa quis. Non reiciendis velit, rem consectetur culpa tenetur quibusdam vero aperiam eligendi repudiandae itaque soluta qui enim earum, dolore veniam sapiente id necessitatibus minus! Saepe aliquam id aspernatur quia eveniet minus doloremque temporibus sed?
     </p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+        <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at blanditiis temporibus voluptatem quia perferendis magnam eaque voluptates culpa recusandae porro illo praesentium reprehenderit ab, quaerat, quod dolorem deserunt doloribus consequuntur in rerum aliquid consectetur illum odio? Eum cumque illo, saepe cum ad, architecto magni, numquam iure voluptates ullam sequi libero voluptas deleniti ducimus incidunt laboriosam sed quas porro! Magni amet nobis nulla explicabo in. Repudiandae beatae saepe impedit laboriosam alias reiciendis amet voluptatibus quod, quibusdam, quo debitis explicabo sapiente sunt nobis ullam mollitia quaerat repellendus. Consequuntur adipisci tempore quas voluptas quasi earum deleniti nostrum minima quod culpa soluta magnam enim doloribus natus molestiae voluptate nihil, accusantium facere accusamus iste, at dolor hic! Neque in sapiente officiis amet ut, quam nesciunt accusantium beatae reprehenderit et unde voluptates nobis nisi illum mollitia enim aspernatur modi assumenda optio iste est aliquid! Ipsam aut alias accusamus, laboriosam nemo cupiditate eveniet magni hic repellendus modi officia delectus, nam recusandae voluptatem, vero veritatis neque deserunt similique quibusdam dolore quam unde culpa quis. Non reiciendis velit, rem consectetur culpa tenetur quibusdam vero aperiam eligendi repudiandae itaque soluta qui enim earum, dolore veniam sapiente id necessitatibus minus! Saepe aliquam id aspernatur quia eveniet minus doloremque temporibus sed?
     </p>
 
+        <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at blanditiis temporibus voluptatem quia perferendis magnam eaque voluptates culpa recusandae porro illo praesentium reprehenderit ab, quaerat, quod dolorem deserunt doloribus consequuntur in rerum aliquid consectetur illum odio? Eum cumque illo, saepe cum ad, architecto magni, numquam iure voluptates ullam sequi libero voluptas deleniti ducimus incidunt laboriosam sed quas porro! Magni amet nobis nulla explicabo in. Repudiandae beatae saepe impedit laboriosam alias reiciendis amet voluptatibus quod, quibusdam, quo debitis explicabo sapiente sunt nobis ullam mollitia quaerat repellendus. Consequuntur adipisci tempore quas voluptas quasi earum deleniti nostrum minima quod culpa soluta magnam enim doloribus natus molestiae voluptate nihil, accusantium facere accusamus iste, at dolor hic! Neque in sapiente officiis amet ut, quam nesciunt accusantium beatae reprehenderit et unde voluptates nobis nisi illum mollitia enim aspernatur modi assumenda optio iste est aliquid! Ipsam aut alias accusamus, laboriosam nemo cupiditate eveniet magni hic repellendus modi officia delectus, nam recusandae voluptatem, vero veritatis neque deserunt similique quibusdam dolore quam unde culpa quis. Non reiciendis velit, rem consectetur culpa tenetur quibusdam vero aperiam eligendi repudiandae itaque soluta qui enim earum, dolore veniam sapiente id necessitatibus minus! Saepe aliquam id aspernatur quia eveniet minus doloremque temporibus sed?
+    </p>
+
+        <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, at blanditiis temporibus voluptatem quia perferendis magnam eaque voluptates culpa recusandae porro illo praesentium reprehenderit ab, quaerat, quod dolorem deserunt doloribus consequuntur in rerum aliquid consectetur illum odio? Eum cumque illo, saepe cum ad, architecto magni, numquam iure voluptates ullam sequi libero voluptas deleniti ducimus incidunt laboriosam sed quas porro! Magni amet nobis nulla explicabo in. Repudiandae beatae saepe impedit laboriosam alias reiciendis amet voluptatibus quod, quibusdam, quo debitis explicabo sapiente sunt nobis ullam mollitia quaerat repellendus. Consequuntur adipisci tempore quas voluptas quasi earum deleniti nostrum minima quod culpa soluta magnam enim doloribus natus molestiae voluptate nihil, accusantium facere accusamus iste, at dolor hic! Neque in sapiente officiis amet ut, quam nesciunt accusantium beatae reprehenderit et unde voluptates nobis nisi illum mollitia enim aspernatur modi assumenda optio iste est aliquid! Ipsam aut alias accusamus, laboriosam nemo cupiditate eveniet magni hic repellendus modi officia delectus, nam recusandae voluptatem, vero veritatis neque deserunt similique quibusdam dolore quam unde culpa quis. Non reiciendis velit, rem consectetur culpa tenetur quibusdam vero aperiam eligendi repudiandae itaque soluta qui enim earum, dolore veniam sapiente id necessitatibus minus! Saepe aliquam id aspernatur quia eveniet minus doloremque temporibus sed?
+    </p>
+    </div>
+    <!-- </div> -->
   </Layout>
 </template>
 
 <script>
+import Carousel from '~/components/Carousel.vue'
+
 export default {
   metaInfo: {
     title: 'Bienvenidos!'
-  }
+  },
+  components: {
+    Carousel
+  },
 }
 </script>
 
 <style>
 .home-links a {
   margin-right: 1rem;
+}
+
+.index-content-container {
+  padding-left: 20px;
+  padding-right: 20px;
 }
 
 </style>
