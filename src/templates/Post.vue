761eaa27f14ad950fac84cca2364b63a6ab10a07
Michael Lamb <mflambjr@gmail.com>
2019-07-01 23:00:32 -0400

    add markdown file folder and vue template for rendering isolated content.  kinda required by gridsome though we may never need it

---
+++ src/templates/Post.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+layout
+  div(v-html="$page.post.content")
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  post: post (path: $path) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo () {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
