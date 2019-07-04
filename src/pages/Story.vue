6735b39d0e88406c88c853457262e9bcba6bbcdc
Michael Lamb <mflambjr@gmail.com>
2019-07-03 22:19:12 -0400

    Pull in Vue and graphql queries for new .md data sources

--- src/pages/Story.vue
+++ src/pages/Story.vue
@@ -1,15 +1,42 @@
-<template>
-  <Layout>
-    <h1>Our Story</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas velit quas, nulla laboriosam itaque quo enim odit animi recusandae nam, unde ratione mollitia exercitationem dolores facilis debitis. Natus exercitationem, nobis consectetur, necessitatibus, nesciunt ab aliquid rerum sint ullam rem quibusdam minus aperiam beatae. Tenetur iste ipsa nihil aliquid voluptatum placeat adipisci est soluta impedit, consequatur blanditiis quae asperiores ad! Aliquid in pariatur consequuntur nemo. Culpa quas, cupiditate dolores porro non, quae exercitationem minus dolore error ratione tempora vero tenetur magni fuga? Quis incidunt quos suscipit nisi minima neque dolores adipisci pariatur laudantium ea qui maiores similique, aut libero? Est accusamus, laudantium nesciunt vitae soluta consequatur sequi veritatis fuga veniam velit? Asperiores ipsam nesciunt incidunt cumque iste quis repudiandae temporibus adipisci quo veniam, velit omnis. Reprehenderit in ipsam itaque possimus eius dolore facilis eligendi. Mollitia labore sit voluptatibus perferendis laboriosam, tempora blanditiis consequatur ad! Beatae recusandae delectus quos hic dolor quae quidem et perspiciatis praesentium fugiat ducimus quis unde rem sint, molestias, fugit, blanditiis odio at? Itaque unde similique ex, enim, eos numquam obcaecati dolore natus inventore error quibusdam. Perferendis impedit, fugit corrupti dolorem inventore deserunt accusantium commodi laboriosam voluptates soluta vero id repellat iste velit! Totam, pariatur, error expedita laboriosam eum delectus perferendis quis tenetur ipsam repellat repellendus eius beatae dolorem et porro? Quidem laudantium iste cumque amet reiciendis voluptas veritatis a consequuntur odio id commodi et fuga distinctio quos tempore similique obcaecati adipisci, quis quae reprehenderit autem. Cum dolore at, totam est dolorum unde! Cum sit repudiandae ullam minus eius consequatur perspiciatis, hic dignissimos alias ducimus iure molestias ipsum ut. Officia culpa atque dolorum impedit hic iste, cumque ipsam! Odit id assumenda quidem suscipit nisi sit animi eaque illo!</p>
-  </Layout>
+<template lang="pug">
+  layout
+    .index-content-container 
+      .title.mb-3
+    
+
+      v-flex(xs12 sm6 offset-sm3)
+        v-card(
+          v-for="(post, index) in posts"
+          :key="index"
+        )
+          v-card-title(primary-title)
+            div(v-html="post.node.content")
 </template>
+
+<page-query>
+query {
+  allPost(filter: { title: { eq: "Our Story" }}) {
+    edges {
+      node {
+        id
+        title
+        content
+        path
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Our Story'
+    title: 'Our Story!'
+  },
+  computed: {
+    posts() {
+      return this.$page.allPost.edges
+    }
   }
 }
 </script>
