e6b8d638d90c139e69ef68dcf8a27d1721916072
Michael Lamb <mflambjr@gmail.com>
2019-07-05 00:12:18 -0400

    component for rendering IG pics

---
+++ src/components/InstagramFeed.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+
+  //- v-flex(xs12 sm6 offset-sm3)
+  //-   v-card(
+  //-     v-for="(post, index) in $static.allInstagramPhoto.edges"
+  //-     :key="index"
+  //-   )
+  //-     v-card-title(primary-title)
+  //-       v-img(
+  //-         :src="post.node.display_url"
+  //-       )
+  v-flex(xs12 sm6 offset-sm3)
+    v-card
+      v-container( v-bind="{ [`grid-list-md`]: true }" fluid)
+        v-layout(row wrap)
+          v-flex(
+            v-for="(post, index) in $static.allInstagramPhoto.edges"
+            :key="index"
+            xs4
+          )
+            v-card(flat tile)
+              v-img(
+                :src="post.node.display_url"
+              )
+</template>
+
+<static-query>
+query {
+  allInstagramPhoto {
+    edges {
+      node {
+        display_url
+        edge_media_to_caption {
+          edges {
+            node {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+
+export default {
+  name: 'InstagramFeed'
+}
+
+</script>
