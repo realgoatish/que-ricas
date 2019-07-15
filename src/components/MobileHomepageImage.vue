87de3eafdc3d0e73655a090cedfb92a72e524a0d
Michael Lamb <mflambjr@gmail.com>
2019-07-14 23:11:37 -0400

    make replacement image a component

---
+++ src/components/MobileHomepageImage.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div(
+    class="mobile-view-img hidden-md-and-up"
+  )
+    v-img(
+      :src="mobileImage"
+    )
+</template>
+
+<script>
+
+export default {
+    name: 'MobileHomepageImage',
+    data () {
+        return {
+          mobileImage: require('../table.jpg')
+        }
+    }
+}
+
+</script>
+
+<style>
+
+.mobile-view-img {
+  width: 100%;
+  height: 400px;
+  display: block;
+  border-radius: 0px 0px 15px 15px;
+}
+
+</style>
