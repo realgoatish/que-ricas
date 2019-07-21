67a5ea250293db89ad7b5cd26c555186db66534c
Michael Lamb <mflambjr@gmail.com>
2019-07-21 15:47:32 -0400

    move footer contact info to a component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,73 +6,48 @@
     v-container(grid-list-sm style="max-width:1300px")
       v-layout(row wrap)
         v-flex(xs12 md4 order-xs1)
-          v-card(
-            flat
-            tile
-            class="white orange--text text--darken-2"
-          )
+          FooterContact
 
-            v-card-text(
-                class="orange--text text--darken-2 pt-3"
-                )
-              strong CONTACT
-              br
-              br
-              div 1234 Xxxxxx Avenue #[br] #[br] Xxxxxx Township, New Jersey 90210 #[br] #[br]quericas2019@gmail.com | 123 456 7891 #[br] #[br] Sunday – Thursday 5 pm to 9 pm #[br] #[br] Friday – Saturday 5 pm to 10 pm
-
-            v-card-text
-              v-btn(
-                v-for="item in icons"
-                :key="item.id"
-                class="white--text"
-                flat
-                icon
-                :href="item.link"
-                target="_blank"
-                color="orange darken-2"
-              )
-                font-awesome(:icon="item.config" size="2x")
-
-        //- v-divider(vertical inset=true)    
 
         v-flex(xs12 md4 order-md2 order-xs3)
           v-img(alt="Que Ricas Logo" :src="logo" :width="width" class="mx-auto")
           v-card(flat tile)
             v-card-text &copy;2019 - #[strong Que Ricas]
           
-        //- v-divider(vertical inset=true)
       
         v-flex(xs12 md4 order-md3 order-xs2)
-          InstagramFeed
+          FooterInstagramFeed
 </template>
 
 <script>
 
-import InstagramFeed from '~/components/InstagramFeed.vue'
+import FooterContact from '~/components/FooterContact.vue'
+import FooterInstagramFeed from '~/components/FooterInstagramFeed.vue'
 
 export default {
     name: 'Footer',
     data () {
         return {
             logo: require("../../static/uploads/que-ricas-logo.png"),
-            width: "300px",
-            icons: {
-                facebook: {
-                    id: 1,
-                    config: ['fab', 'facebook'],
-                    link: 'https://www.facebook.com/quericashaddon/'
-                },
-                instagram: {
-                    id: 2,
-                    config: ['fab', 'instagram'],
-                    link: 'https://www.instagram.com/quericas_haddon/?hl=en'
-                },
-            }
+            width: "300px"
+            // icons: {
+            //     facebook: {
+            //         id: 1,
+            //         config: ['fab', 'facebook'],
+            //         link: 'https://www.facebook.com/quericashaddon/'
+            //     },
+            //     instagram: {
+            //         id: 2,
+            //         config: ['fab', 'instagram'],
+            //         link: 'https://www.instagram.com/quericas_haddon/?hl=en'
+            //     }
+            // }
             
         }
     },
     components: {
-        InstagramFeed
+      FooterContact,
+      FooterInstagramFeed
     }
 }
 
---
+++ src/components/FooterContact.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+
+  v-card(
+    flat
+    tile
+    class="white orange--text text--darken-2"
+  )
+
+    v-card-text(
+      class="orange--text text--darken-2 pt-3"
+      v-for="(post, index) in posts"
+      :key="index"
+    )
+      strong(
+        v-html="post.node.excerpt"
+        style="font-size:18px;"
+      )
+      br
+      br
+      div(
+        v-html="post.node.content"
+        style="font-size:16px;"
+      )
+
+    v-card-text
+      v-btn(
+        v-for="item in icons"
+        :key="item.id"
+        class="white--text"
+        flat
+        icon
+        :href="item.link"
+        target="_blank"
+        color="orange darken-2"
+      )
+        font-awesome(:icon="item.config" size="2x")
+
+</template>
+
+<static-query>
+
+query {
+  allPost(filter: { excerpt: { eq: "CONTACT" }}) {
+    edges {
+      node {
+        id
+        excerpt
+        title
+        content
+        path
+      }
+    }
+  }
+}
+
+</static-query>
+
+<script>
+
+export default {
+    name: 'FooterContact',
+    data () {
+        return {
+            icons: {
+                facebook: {
+                    id: 1,
+                    config: ['fab', 'facebook'],
+                    link: 'https://www.facebook.com/quericashaddon/'
+                },
+                instagram: {
+                    id: 2,
+                    config: ['fab', 'instagram'],
+                    link: 'https://www.instagram.com/quericas_haddon/?hl=en'
+                }
+            }
+        }
+    },
+    computed: {
+        posts() {
+            return this.$static.allPost.edges
+        }
+    }
+}
+
+</script>
