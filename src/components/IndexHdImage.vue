1666fc53328df54b00cbd143b6564914f39e6493
Michael Lamb <mflambjr@gmail.com>
2019-07-22 16:54:28 -0400

    refactor content area image and text into components

---
+++ src/components/IndexHdImage.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  g-image(
+    :src="hdImage"
+    alt="Honestly Delicious.  Local.  Better ingredients.  Authentic."
+    class="hd-image-class"
+    width="auto"
+    height="auto"
+    fit="outside"
+  )
+</template>
+
+<script>
+
+export default {
+    name: 'IndexHdImage',
+    data () {
+      return {
+        hdImage: require("../../static/uploads/hd.png")
+    }
+  }
+}
+
+</script>
+
+<style>
+.hd-image-class {
+  margin: 0 auto;
+}
+</style>
