2b5c8515a411eb2cc72be944511c203cabcffe4e
Michael Lamb <mflambjr@gmail.com>
2019-07-23 19:15:03 -0400

    configure self-hosting of Material Design Icons

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+// Self-host Material Design Icons
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vuetify from 'vuetify'
 import DefaultLayout from '~/layouts/Default.vue'
 
 import 'vuetify/dist/vuetify.min.css'
 
 export default function (Vue, { router, head, isClient }) {
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
-  })
+  // head.link.push({
+  //   rel: 'stylesheet',
+  //   href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+  // })
 
-  Vue.use(Vuetify)
+  Vue.use(Vuetify, {
+    iconfont: 'md'
+  })
   
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
