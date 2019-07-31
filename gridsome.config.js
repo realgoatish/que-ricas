4d8685168b021f69e53b5d2eac6819afcc516d6c
Michael Lamb <mflambjr@gmail.com>
2019-07-30 23:02:48 -0400

    configure purgecss plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -82,6 +82,20 @@ module.exports = {
         username: 'quericas_haddon',
         typeName: 'InstagramPhoto'
       }
+    },
+    {
+      use: 'gridsome-plugin-purgecss',
+      // default options, the following will be included if you don't provide anything
+      options: {
+        content: [
+          './src/**/*.vue',
+          './src/**/*.js',
+          './src/**/*.jsx',
+          './src/**/*.pug',
+          './src/**/*.md'
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }
     }
   ],
   chainWebpack: config => {
