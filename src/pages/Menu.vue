d400299f250b18e51f5cae1dc84d7a00d6166f41
Michael Lamb <mflambjr@gmail.com>
2019-08-07 10:02:30 -0400

    replace menu image with 'under construction' component

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -1,32 +1,19 @@
 <template lang="pug">
   layout
-    .index-content-container
-      .title.mb-3
-    
-      main
-        v-flex(xs12 md8 offset-md2)
-          v-card
-            v-card-title(primary-title)
-              v-img(
-                :src="menuImage"
-                alt="Image of Que Ricas Menu"
-                width="auto"
-                height="auto"
-                fit="fill"
-              )
-      .title.mb-3
+    MenuUnderConstruction
 
 </template>
 
 <script>
+
+import MenuUnderConstruction from '~/components/MenuUnderConstruction.vue';
+
 export default {
   metaInfo: {
     title: 'Menu'
   },
-  data () {
-    return {
-      menuImage: 'https://res.cloudinary.com/caribou-images/image/upload/q_auto,f_auto/menu_ctermw.jpg'
-    }
+  components: {
+    MenuUnderConstruction
   }
 }
 </script>
