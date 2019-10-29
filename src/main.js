0dc12e52aa3e0787d0e6d9f6b48bb8dbcecfc740
Michael Lamb <mflambjr@gmail.com>
2019-10-28 21:44:48 -0400

    remove global 'rel=canonical' and OG tags that should be done on each page

--- src/main.js
+++ src/main.js
@@ -12,21 +12,6 @@ import DefaultLayout from '~/layouts/Default.vue';
 
 export default function (Vue, { router, head, isClient }) {
 
-  head.link.push({
-    rel: 'canonical',
-    href: 'https://www.que-ricas.com/'
-  })
-
-  // head.meta.push({
-  //   name: 'google-site-verification',
-  //   content: '0hx-doVB8lRgf57e9KmtL8yE4fbehtdGAybDCeBNhYs'
-  // })
-
-  // head.meta.push({
-  //   name: 'google-site-verification',
-  //   content: 'lSkgvmZFVpT1PSa-HD1driBcKzJdPiepFh3dtcAKFCg'
-  // })
-
   head.meta.push({
     property: 'og:locale',
     content: 'en-US'
