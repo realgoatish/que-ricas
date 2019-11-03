260487f06f4258db82b61840a7ac7dae7b3e8acf
Michael Lamb <mflambjr@gmail.com>
2019-11-03 09:43:22 -0500

    have to use 2 nearly identical components for now due to g-image problems with accepting props

---
+++ src/components/404/ErrorWidget.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+
+  v-card
+    v-card-title(primary-title)
+      div(class="container")
+        div
+          p(class="custom-font") {{ headerText }}
+      g-image(
+        src="~/../uploads/woops-pusheen-cat.png"
+        :alt="imageAlt"
+        width="200"
+        height="200"
+        fit="fill"
+      )
+      div(class="container")
+        div
+          p(class="sub-text") {{ errorMessage }}
+            
+
+</template>
+
+<script>
+export default {
+  name: 'ErrorWidget',
+  data () {
+    return {
+      headerText: 'Woops!',
+      imageAlt: "Picture of pusheen cat who put his face through a loaf of bread trying to find a page that doesn't exist",
+      errorMessage: "The server returned a 404 error because this page doesn't exist!"
+    }
+  },
+}
+</script>
+
+<style>
+
+/* @font-face kit by Fonts2u (https://fonts2u.com) */ @font-face {font-family:"Beach Resort";src:url("../../fonts/Beach_Resort.eot?") format("eot"),url("../../fonts/Beach_Resort.woff") format("woff"),url("../../fonts/Beach_Resort.ttf") format("truetype"),url("../../fonts/Beach_Resort.svg#BeachResort") format("svg");font-weight:normal;font-style:normal;}
+
+
+.custom-font {
+  font-family: "Beach Resort";
+  font-size: 4em;
+  display: inline-block;
+  text-align: center;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.g-image {
+  display: block;
+  margin: 0 auto;
+}
+
+.container {
+  display: inline-block;
+}
+
+.sub-text {
+  font-size: 2.2em;
+  text-align: center;
+}
+
+</style>
---
+++ src/components/menu/MenuNoticeWidget.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+
+  v-card
+    v-card-title(primary-title)
+      div(class="container")
+        div
+          p(class="custom-font") {{ headerText }}
+      g-image(
+        src="~/../uploads/trabajador.png"
+        :alt="imageAlt"
+        width="150"
+        height="200"
+        fit="fill"
+      )
+      div(class="container")
+        div
+          p(class="sub-text") {{ mainText }}
+            
+
+</template>
+
+<script>
+export default {
+  name: 'MenuNoticeWidget',
+  data () {
+    return {
+      headerText: "Coming Soon!",
+      imageAlt: "Cartoon Venezuelan worker carrying a pickaxe",
+      mainText: "Our menu is still under construction..."
+    }
+  }
+}
+</script>
+
+<style>
+
+/* @font-face kit by Fonts2u (https://fonts2u.com) */ @font-face {font-family:"Beach Resort";src:url("../../fonts/Beach_Resort.eot?") format("eot"),url("../../fonts/Beach_Resort.woff") format("woff"),url("../../fonts/Beach_Resort.ttf") format("truetype"),url("../../fonts/Beach_Resort.svg#BeachResort") format("svg");font-weight:normal;font-style:normal;}
+
+
+.custom-font {
+  font-family: "Beach Resort";
+  font-size: 4em;
+  display: inline-block;
+  text-align: center;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.g-image {
+  display: block;
+  margin: 0 auto;
+}
+
+.container {
+  display: inline-block;
+}
+
+.sub-text {
+  font-size: 2.2em;
+  text-align: center;
+}
+
+</style>
