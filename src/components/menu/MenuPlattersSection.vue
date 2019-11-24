30512baf182a8daf98a6737edd7a57bd6fcad9ad
Michael Lamb <mflambjr@gmail.com>
2019-11-24 18:27:19 -0500

    add blurbs after main section header

--- src/components/menu/MenuPlattersSection.vue
+++ src/components/menu/MenuPlattersSection.vue
@@ -3,6 +3,9 @@
   v-card
     v-card-title(primary-title)
       h2(class="menu-section-header") Platters
+      article
+        div(class="menu-section-header" style="font-size:1.2em;")
+          p Customize your own delicious platter with your choice of protein and style!
       h2(class="sub-header-text") Step One: Choose Your Protein
       MenuPlattersStepOne
       h2(class="sub-header-text") Step Two: Choose Your Style
