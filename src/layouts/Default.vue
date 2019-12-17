ac696265c1d2dac59c74f8444342aa1adbae6f2e
Michael Lamb <mflambjr@gmail.com>
2019-12-16 20:27:19 -0500

    simple formatting

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -41,8 +41,8 @@ query {
 
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
