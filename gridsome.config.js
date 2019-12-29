91e59a0638a52f31269505a9269d0a9df8c8b950
Michael Lamb <mflambjr@gmail.com>
2019-12-29 01:57:52 -0500

    take out sitemap plugin config.  Custom was needed for the config I want

--- gridsome.config.js
+++ gridsome.config.js
@@ -41,43 +41,6 @@ module.exports = {
         height: 900
       }
     },
-    // {
-    //   use: '@gridsome/plugin-sitemap',
-    //   options: {
-    //     cacheTime: 600000,
-    //     staticUrls: [
-    //       {
-    //         url: '/assets/static/',
-    //         img: [
-    //           {
-    //             url: '/assets/static/cartel.09b17d9.84cd0cf5572e6dd571ede737e6427cd5.jpeg',
-    //             caption: 'chorizo, carmelized peppers, onions, smoked gouda',
-    //             title: 'The Cartel Arepa',
-    //             geoLocation: 'Haddon Township, New Jersey, USA'
-    //           },
-    //           {
-    //             url: '/assets/static/hangover-bowl.09b17d9.54dbf4efe6f2dfbbd5930c10fb1a3748.jpeg',
-    //             caption: 'shredded beef, yuca hash browns, fried egg, cilantro mojo',
-    //             title: 'The Shredded Beef Hangover Bowl Platter',
-    //             geoLocation: 'Haddon Township, New Jersey, USA'
-    //           },
-    //           {
-    //             url: '/assets/static/sahar-our-story.09b17d9.af141ba626217e0822a79c7598953243.jpeg',
-    //             caption: 'Sahar Soleymani',
-    //             title: 'Que Ricas Founder Sahar Soleymani',
-    //             geoLocation: 'Haddon Township, New Jersey, USA'
-    //           },
-    //           {
-    //             url: '/assets/static/v-dar-elsie.e18b67c.e2c7e697046ee47f7693d2eade00d302.jpg',
-    //             caption: 'Dar and Elsie Soleymani',
-    //             title: 'Dar and Elsie Soleymani',
-    //             geoLocation: 'Haddon Township, New Jersey, USA'
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   }
-    // },
     {
       use: '@zefman/gridsome-source-instagram',
       options: {
